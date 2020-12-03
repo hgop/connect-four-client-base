a15d74a52b6008dcc297c689899cc5f74ba9d3f7
fanneyyy <fanneyyyy@gmail.com>
2020-12-03 21:30:20 +0000

    Change api url

--- src/external_services/game_api_client.ts
+++ src/external_services/game_api_client.ts
@@ -37,7 +37,7 @@ export interface Game {
   playerCount: number;
 }
 
-const API_URL = "http://game-server.dreamteam.hgopteam.com";
+const API_URL = "GET_URL_FROM_TAS";
 
 export class GameApiClient {
   createGame(body: CreateGame): Promise<Game> {
